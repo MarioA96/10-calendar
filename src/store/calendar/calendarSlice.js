e6d2b104de9e46fdf246dfaf8357d3e945da2b14
MarioA96 <marioaquirozs@gmail.com>
2024-04-08 21:16:51 -0600

    Se actualizaron archivos
    Se actualizaron los archivos del directorio store calendarSlice.js y store.js

--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -3,11 +3,12 @@ import { createSlice } from '@reduxjs/toolkit';
 import { addHours } from 'date-fns';
 
 const tempEvent = {
+    _id: new Date().getTime(),
     title: 'Cumpleaños del Jefe',
     notes: 'Comprar un regalo',
     start: new Date(),
     end: addHours( new Date(), 2 ),
-    bgColor: '#fafafa',
+    bgColor: '#fafafa', //Aqui podria ir un color random o un color por usuario
     user: {
         _id: '123',
         name: 'Mario'
@@ -23,12 +24,31 @@ export const calendarSlice = createSlice({
         activeEvent: null,
     },
     reducers: {
-        increment: (state, /* action */ ) => {
-            state.counter += 1;
+        onSetActiveEvent: (state, { payload }) => {
+            state.activeEvent = payload;
+        },
+        onAddNewEvent: (state, { payload }) => {
+            state.events.push( payload );
+            state.activeEvent = null;
+        },
+        onUpdateEvent: ( state, { payload } ) => {
+            state.events = state.events.map( e => {
+                if( e._id === payload._id ){
+                    return payload;
+                }
+                
+                return e;
+            } );
+        },
+        onDeleteEvent: ( state ) => {
+            if( state.activeEvent ){
+                state.events = state.events.filter( e => e._id !== state.activeEvent._id );
+                state.activeEvent = null;
+            }
         },
     }
 });
 
 
 // Action creators are generated for each case reducer function
-export const { increment } = calendarSlice.actions;
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent } = calendarSlice.actions;
